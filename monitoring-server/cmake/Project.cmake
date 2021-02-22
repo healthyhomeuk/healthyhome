40da78fd9012b9c4c0dbc9a564a89af772e456f2
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-02-22 13:02:20 +0000

    monit/cmake: Add project options target
    Add a project options target so that all the required options can be grouped together and reused throughout all the executable platforms.

--- monitoring-server/cmake/Project.cmake
+++ monitoring-server/cmake/Project.cmake
@@ -3,7 +3,8 @@ add_library(Core)
 
 target_link_libraries(Core
         PRIVATE
-        project_warnings)
+        project_warnings
+        project_options)
 
 set_target_properties(Core PROPERTIES LINKER_LANGUAGE CXX)
 
