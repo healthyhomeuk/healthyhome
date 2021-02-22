2c56048a2da345e1af3a9440f6747189d1b521c7
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-02-22 13:13:36 +0000

    monit/core: Create defs.h
    Creates a basic definitions file for the core framework of the monitoring server.

---
+++ monitoring-server/include/core/defs.h
@@ -0,0 +1,65 @@
+/*
+ * This file is part of the HealthyHome project monitoring server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file core/defs.h
+ */
+
+#ifndef CORE_DEFS_H
+#define CORE_DEFS_H
+
+namespace Core {
+
+    /**
+     * @brief Operations status codes type
+     * @headerfile core/defs.h <core/defs.h>
+     *
+     * This enumerator helps keeping consistency in
+     * operation statuses throughout the whole project.
+     * Any method that performs an operation a returns
+     * a status code, should use this enumerator type.
+     *
+     * Usage example:
+     * \code{.cpp}
+     * Core::StatusCode computation(int a, b) {
+     *     // perform super long computation
+     *     return Core::SUCCESS;
+     * }
+     * \endcode
+     */
+    enum StatusCode {
+        /**
+         * returned if the operation was successful.
+         */
+        SUCCESS,
+        /**
+         * returned if the operation was not performed
+         * due to the given resource being busy.
+         */
+        E_BUSY,
+        /**
+         * returned if the operation was not performed
+         * due to a conflict with the given resource.
+         */
+        E_CONFLICT
+    };
+
+}
+
+#endif // CORE_DEFS_H
