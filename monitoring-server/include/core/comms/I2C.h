a112293ae1cbe87233073140e39718c5701da5e2
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-02-22 13:26:02 +0000

    monit/core: Create Device class definition
    Create the Device abstract class in the core framework of the monitoring server project.

---
+++ monitoring-server/include/core/comms/I2C.h
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the HealthyHome project monitoring server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file core/comms/I2C.h
+ */
+
+#ifndef CORE_COMMS_I2C_H
+#define CORE_COMMS_I2C_H
+
+#include <core/comms/Packet.h>
+
+namespace Core::Comms {
+
+    /**
+     * @brief Communication interface for I2C.
+     * @headerfile core/comms/I2C.h <core/comms/I2C.h>
+     */
+    class I2C {
+    public:
+        /**
+         * @brief Method for reading a block of data.
+         * @param deviceId I2C Device 7-bit identifier
+         * @param command I2C Command 8-bit identifier
+         * @param packet A reference to the packet to read.
+         * @return Status operation code.
+         */
+        virtual StatusCode read(unsigned char deviceId, unsigned char command, Packet &packet) = 0;
+
+        /**
+         * @brief Method for writing a block of data.
+         * @param deviceId I2C Device 7-bit identifier
+         * @param command I2C Register 8-bit identifier
+         * @param packet A reference to the packet to write.
+         * @return Status operation code.
+         */
+        virtual StatusCode write(unsigned char deviceId, unsigned char command, Packet &packet) = 0;
+    };
+
+}
+
+#endif // CORE_COMMS_I2C_H
---
+++ monitoring-server/include/core/comms/Packet.h
@@ -0,0 +1,118 @@
+/*
+ * This file is part of the HealthyHome project monitoring server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file core/comms/Packet.h
+ */
+
+#ifndef CORE_COMMS_PACKET_H
+#define CORE_COMMS_PACKET_H
+
+#include <cstdint>
+
+namespace Core::Comms {
+
+    /**
+     * @brief An interface for a data structure containing information
+     *        for a hardware-based communication.
+     * @headerfile core/comms/Packet.h <core/comms/Packet.h>
+     *
+     * Example:
+     * @code{.cpp}
+     * class TemperaturePacket : Core::Packet {
+     * public:
+     *     float temperature;
+     *
+     *     size_t getBufferSize() override {
+     *         return sizeof(temperature); // 4 bytes
+     *     }
+     *
+     *     Core::StatusCode deserialize(const uint8_t *buffer) override {
+     *         // buffer is 4 bytes as per getSize()
+     *         // MSB[0]...LSB[3]
+     *         uint32_t tmp = 0;
+     *         tmp |= buffer[0] << 24;
+     *         tmp |= buffer[1] << 16;
+     *         tmp |= buffer[2] << 8;
+     *         tmp |= buffer[3];
+     *         temperature = *reinterpret_cast<float*>(&tmp);
+     *
+     *         return Core::SUCCESS;
+     *     }
+     *
+     *     Core::StatusCode serialize(uint8_t *buffer) override {
+     *         // buffer is 4 bytes as per getSize()
+     *         // MSB[0]...LSB[3]
+     *         uint32_t tmp = *reinterpret_cast<uint32_t*>(&temperature);
+     *         buffer[0] = tmp >> 24 & 0xFF;
+     *         buffer[1] = tmp >> 16 & 0xFF;
+     *         buffer[2] = tmp >> 8 & 0xFF;
+     *         buffer[3] = tmp & 0xFF;
+     *     }
+     * };
+     * @endcode
+     */
+    class Packet {
+    public:
+
+        /**
+         * @brief Buffer size
+         *
+         * The buffer size required for the deserialization/serialization
+         * process of the Packet. In other words, the number of bytes
+         * that are required to read/write the whole Packet.
+         *
+         * @return The size of the buffer in bytes.
+         */
+        virtual size_t getBufferSize() = 0;
+
+        /**
+         * @brief Deserialization method
+         *
+         * This method is meant to deserialize a Packet from a given
+         * buffer of size given by the getter getBufferSize().
+         *
+         * For example if the Packet represents temperature data
+         * represented by a IEEE 754 single-precision floating point
+         * number, the buffer could be 4-bytes-long containing the
+         * number split in bytes in a known endianness.
+         *
+         * @param buffer The buffer to deserialize the Packet from.
+         */
+        virtual StatusCode deserialize(const uint8_t *buffer) = 0;
+
+        /**
+         * @brief Serialization method
+         *
+         * This method is meant to serialize a Packet to a given
+         * buffer of size given by the getter getBufferSize().
+         *
+         * For example if the Packet represents temperature data
+         * represented by a IEEE 754 single-precision floating point
+         * number, the buffer could be 4-bytes-long containing the
+         * number split in bytes in a known endianness.
+         *
+         * @param buffer The buffer to serialize the Packet to.
+         */
+        virtual StatusCode serialize(uint8_t *buffer) = 0;
+    };
+
+}
+
+#endif // CORE_COMMS_PACKET_H
