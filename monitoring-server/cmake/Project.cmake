837137fe0cc497cfca9844ef5bee42cce00bcaa1
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-02-22 13:01:03 +0000

    monit/cmake: Add compiler warnings options
    Adds all the suitable compiler warnings options in the CMake monitoring server project. Eventually these are also treated as errors, so that no warnings are allowed to pass.

--- monitoring-server/cmake/Project.cmake
+++ monitoring-server/cmake/Project.cmake
@@ -1,6 +1,10 @@
 
 add_library(Core)
 
+target_link_libraries(Core
+        PRIVATE
+        project_warnings)
+
 set_target_properties(Core PROPERTIES LINKER_LANGUAGE CXX)
 
 target_include_directories(Core
