949f468730958fa95ab5608060e0b5decbfff36b
Amirul Lokman <amirul.l.jamaludin@gmail.com>
2021-03-09 23:23:46 +0000

    app/views: Add Settings screen
    The settings screen will allow users to customise and configure the app.

---
+++ mobile-app/src/views/Settings.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+/*
+ * This file is part of the HealthyHome project monitoring server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Renders the Settings screen.
+ */
+function Settings() {
+    return(
+        <View style= {styles.container}>
+            <Text style={styles.text}>This is the Settings screen.</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f2ffea'
+    },
+    text: {
+      color: '#101010',
+      fontSize: 24,
+      fontWeight: 'bold'
+    }
+  })
+
+export default Settings;
