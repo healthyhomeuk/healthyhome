78711e713323a315442e2ab98ba40f7fa9a4b07c
Amirul Lokman <amirul.l.jamaludin@gmail.com>
2021-03-12 17:40:10 +0000

    app/views: Fix formatting

--- mobile-app/src/App.js
+++ mobile-app/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import MainNavigationBar from './navigation/MainNavigationBar';
-
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MainNavigationBar from "./navigation/MainNavigationBar";
 
 /**
  * Renders all the components of the app.
  */
 export default function App() {
-  return (
-    <NavigationContainer>
-    <MainNavigationBar/>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <MainNavigationBar />
+        </NavigationContainer>
+    );
 }
