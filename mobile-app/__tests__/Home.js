c7766f07401b82007d513bb7644f30137705db19
Amirul Lokman <amirul.l.jamaludin@gmail.com>
2021-03-15 22:58:33 +0000

    app/tests: Reorganise test folders
    The test folders have been moved to its respective view or component for
    a more organised folder structure

--- mobile-app/__tests__/Home.js
+++
@@ -1,9 +0,0 @@
-import 'react-native';
-import React from 'react';
-import Home from '../src/views/Home';
-
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-    renderer.create(<Home />);
-  });
