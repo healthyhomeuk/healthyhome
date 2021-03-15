91a7f4ef1200f6f394e8589c0ebcdab8bf2b7c06
Amirul Lokman <amirul.l.jamaludin@gmail.com>
2021-03-15 22:08:02 +0000

    app/tests: Fix test packages
    Changing the preset for Jest to work with Expo

--- mobile-app/__tests__/App.js
+++ mobile-app/__tests__/App.js
@@ -1,10 +1,12 @@
-import 'react-native';
-import React from 'react';
-import App from '../src/App';
+import "react-native";
+import React from "react";
+import App from "../src/App";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it("renders correctly", async () => {
+    const { getByText } = render(<App />);
+    await waitFor(() => getByText("Home"));
 });
