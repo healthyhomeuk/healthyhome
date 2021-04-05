6098bca868bbf06c2b30af74ec21d0e16cadef0a
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-04-05 18:34:16 +0100

    monit: Ease compiler warnings

--- monitoring-server/cmake/CompilerWarnings.cmake
+++ monitoring-server/cmake/CompilerWarnings.cmake
@@ -40,12 +40,10 @@ function(set_project_warnings project_name)
             -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
             # catch hard to track down memory errors
             -Wold-style-cast # warn for c-style casts
-            -Wcast-align # warn for potential performance problem casts
             -Wunused # warn on anything being unused
             -Woverloaded-virtual # warn if you overload (not override) a virtual function
             -Wpedantic # warn if non-standard C++ is used
             -Wconversion # warn on type conversions that may lose data
-            -Wsign-conversion # warn on sign conversions
             -Wnull-dereference # warn if a null dereference is detected
             -Wdouble-promotion # warn if float is implicit promoted to double
             -Wformat=2 # warn on security issues around functions that format output (ie printf)
@@ -62,7 +60,6 @@ function(set_project_warnings project_name)
             -Wduplicated-cond # warn if if / else chain has duplicated conditions
             -Wduplicated-branches # warn if if / else branches have duplicated code
             -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
-            -Wuseless-cast # warn if you perform a cast to the same type
             )
 
     if (MSVC)
