4b388bf17d28c87070dfa9e18d936055ad4054c6
Rory Ortlepp <2196143O@student.gla.ac.uk>
2021-04-09 21:23:28 +0100

    monit/sn-gcja5: Implement PMSensor

---
+++ monitoring-server/libs/sn-gcja5/src/PMSensor.cpp
@@ -0,0 +1,67 @@
+/*
+ * This file is part of the HealthyHome project monitoring server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <core/exceptions.h>
+#include <sn-gcja5/DensityPacket.h>
+#include <sn-gcja5/PMSensor.h>
+#include <sn-gcja5/ParticleCountPacket.h>
+
+using namespace SNGCJA5;
+
+void PMSensor::read()
+{
+    DensityPacket densityPacket;
+    ParticleCountPacket particlePacket;
+
+    config.i2c.read(I2C_ADDR, densityRegister, densityPacket);
+    config.i2c.read(I2C_ADDR, particleCountRegister, particlePacket);
+
+    density       = densityPacket.density;
+    particleCount = particlePacket.count;
+}
+
+Core::StatusCode PMSensor::setup()
+{
+    return Core::SUCCESS;
+}
+
+Core::StatusCode PMSensor::halt()
+{
+    return Core::SUCCESS;
+}
+
+const char* PMSensor::getName()
+{
+    return sensorName;
+}
+
+std::unique_ptr<Core::Message> PMSensor::handleMessage(
+    std::unique_ptr<Core::Message>)
+{
+    throw Core::Exception::NotFound { "the requested message was not found" };
+}
+
+uint16_t PMSensor::getParticleCount() const
+{
+    return particleCount;
+}
+float PMSensor::getDensity() const
+{
+    return density;
+}
