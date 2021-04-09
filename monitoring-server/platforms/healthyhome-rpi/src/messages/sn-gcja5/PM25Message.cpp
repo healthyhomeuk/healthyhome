e8086a4c23485e68298a373c140d809045e3f046
Rory Ortlepp <2196143O@student.gla.ac.uk>
2021-04-09 21:27:53 +0100

    monit/healthyhome-rpi: Implement PM25Message ZMQ serialization

---
+++ monitoring-server/platforms/healthyhome-rpi/src/messages/sn-gcja5/PM25Message.cpp
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the HealthyHome project monitoring server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <sn-gcja5/PM25Message.h>
+#include <zmq-postman/Postman.h>
+
+using namespace SNGCJA5;
+
+static void serializer(const PM25Message& self, void* payload)
+{
+    auto* msgs = ZmqPostman::Postman::castPayload(payload);
+    msgs->insert({ "particleCount", std::to_string(self.particleCount) });
+    msgs->insert({ "density", std::to_string(self.density) });
+}
+
+static void deserializer(PM25Message&, const void*) { }
+
+static Core::StatusCode setup = [] {
+    PM25Message::SERIALIZER   = serializer;
+    PM25Message::DESERIALIZER = deserializer;
+    return Core::SUCCESS;
+}();
