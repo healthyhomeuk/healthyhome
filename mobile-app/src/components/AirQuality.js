d980eaaa6abe78608db0bc86b7b110cceebf46f4
Amirul Lokman <amirul.l.jamaludin@gmail.com>
2021-04-11 18:26:32 +0100

    app/card: Fix component positioning
    The positioning for the message in the air quality component as well as the arc gradient was not centered properly. This commit fixes it.

--- mobile-app/src/components/AirQuality.js
+++ mobile-app/src/components/AirQuality.js
@@ -68,8 +68,9 @@ const styles = StyleSheet.create({
         alignItems: "center",
     },
     airQualityStatus: {
+        flex: 1,
         width: "100%",
-        height: "15%",
+        height: "20%",
         justifyContent: "center",
         alignItems: "center",
         borderBottomLeftRadius: 24,
