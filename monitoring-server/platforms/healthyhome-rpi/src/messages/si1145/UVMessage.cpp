94c9aefef4706d5d5838e2431afb8fbda31a4b8b
Rory Ortlepp <2196143O@student.gla.ac.uk>
2021-04-12 19:28:27 +0100

    monit/healhyhome-rpi: Implement UVMessage serializer

---
+++ monitoring-server/platforms/healthyhome-rpi/src/messages/si1145/UVMessage.cpp
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the HealthyHome project monitoring server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <si1145/UVMessage.h>
+#include <zmq-postman/Postman.h>
+
+using namespace SI1145;
+
+static void serializer(const UVMessage& self, void* payload)
+{
+    auto* msgs = ZmqPostman::MessageBody::castPayload(payload);
+    msgs->putTagged("value", self.value);
+}
+
+static void deserializer(UVMessage&, const void*) { }
+
+static Core::StatusCode setup = [] {
+    UVMessage::SERIALIZER   = serializer;
+    UVMessage::DESERIALIZER = deserializer;
+    return Core::SUCCESS;
+}();
