e111460e2dbb65d460294c1c16bb5e1b62f4b6be
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-04-14 15:37:15 +0100

    mgmt: Create SN-GCJA5 PM2.5 sensor

---
+++ management-server/src/sensors/sn-gcja5/pm25.ts
@@ -0,0 +1,50 @@
+/*
+ * This file is part of the HealthyHome project management server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Parameter, Quality, QualityTable } from "../types";
+import { DENSITY_PARAMETER, PARTICLE_COUNT_PARAMETER } from "./params";
+import Sensor from "../Sensor";
+
+/** PM2.5 sensor identifier */
+const PM25_SENSOR_ID = "pm25";
+/** PM2.5 sensor textual name */
+const PM25_SENSOR_NAME = "PM2.5";
+
+/** PM2.5 particle count parameter quality levels table */
+const PM25_COUNT_PARAM_QUALITY_TABLE: QualityTable = [
+    [Quality.EXCELLENT, 0, 546],
+    [Quality.FAIR, 546, 1363],
+    [Quality.BAD, 1363, Infinity],
+];
+
+/**
+ * PM2.5 parameters table
+ */
+const PM25_PARAMETERS: Parameter[] = [
+    {
+        ...PARTICLE_COUNT_PARAMETER,
+        qualityTable: PM25_COUNT_PARAM_QUALITY_TABLE,
+    },
+    DENSITY_PARAMETER,
+];
+
+/**
+ * PM2.5 sensor object
+ */
+export default new Sensor(PM25_SENSOR_ID, PM25_SENSOR_NAME, PM25_PARAMETERS);
