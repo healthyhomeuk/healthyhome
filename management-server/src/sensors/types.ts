0aec2b4e018f43b1ef0e1bbefce351ed8fa49c66
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-04-14 15:35:01 +0100

    mgmt: Define Sensor types

---
+++ management-server/src/sensors/types.ts
@@ -0,0 +1,95 @@
+/*
+ * This file is part of the HealthyHome project management server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Quality level enumeration
+ */
+export enum Quality {
+    /** Unknown quality level */
+    UNKNOWN = "UNKNOWN",
+    /** Severe quality level */
+    SEVERE = "SEVERE",
+    /** Very bad quality level */
+    VERY_BAD = "VERY_BAD",
+    /** Bad quality level */
+    BAD = "BAD",
+    /** Poor quality level */
+    POOR = "POOR",
+    /** Fair quality level */
+    FAIR = "FAIR",
+    /** Good quality level */
+    GOOD = "GOOD",
+    /** Excellent quality level */
+    EXCELLENT = "EXCELLENT",
+}
+
+/**
+ * Number type enumeration
+ */
+export enum NumberType {
+    /** Integer number type */
+    INTEGER = "INTEGER",
+    /** Float number type */
+    FLOAT = "FLOAT",
+}
+
+/**
+ * Quality boundary formed of:
+ * - Quality level
+ * - Lower boundary (inclusive)
+ * - Upper boundary (exclusive)
+ *
+ * @example
+ * // An excellent level of `0 <= value < 100` is defined as:
+ * const boundary: QualityBoundary = [Quality.EXCELLENT, 0, 100];
+ */
+export type QualityBoundary = [Quality, number, number];
+
+/**
+ * Quality table in order of either:
+ * - From smallest to largest range
+ * - From lowest to highest range
+ */
+export type QualityTable = QualityBoundary[];
+
+/**
+ * Value-quality pair
+ */
+export type ValueQualityPair = {
+    /** Sensor numeric value */
+    value: number;
+    /** Sensor quality level value */
+    quality: Quality;
+};
+
+/**
+ * Definition of a sensor parameter
+ */
+export interface Parameter {
+    /** Parameter identifier */
+    readonly id: string;
+    /** Parameter textual name */
+    readonly name: string;
+    /** Parameter measurement unit */
+    readonly unit: string;
+    /** Parameter quality levels table */
+    readonly qualityTable: QualityTable;
+    /** Parameter value number type */
+    readonly valueType: NumberType;
+}
