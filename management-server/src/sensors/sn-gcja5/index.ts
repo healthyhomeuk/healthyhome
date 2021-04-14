5457a2c30aab9a1b56ed8aefa7c81c8ee5b57f30
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-04-14 15:37:31 +0100

    mgmt: Create SN-GCJA5 module

---
+++ management-server/src/sensors/sn-gcja5/index.ts
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the HealthyHome project management server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import pm25 from "./pm25";
+import pm100 from "./pm100";
+
+/**
+ * SN-GCJA5 sensors exported by their identifier
+ */
+export default {
+    [pm25.id]: pm25,
+    [pm100.id]: pm100,
+};
