2dfd307d9339f2091af25ad49ad316d791f26164
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-04-14 15:36:49 +0100

    mgmt: Create SI1145 module

---
+++ management-server/src/sensors/si1145/index.ts
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the HealthyHome project management server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import uv from "./uv";
+import visible from "./visible";
+
+/**
+ * SI1145 sensors exported by their identifier
+ */
+export default {
+    [uv.id]: uv,
+    [visible.id]: visible,
+};
