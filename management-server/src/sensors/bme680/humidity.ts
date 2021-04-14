d9e74013b97dd9344243fc515a36b56c9827692f
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-04-14 15:37:53 +0100

    mgmt: Create BME680 Humidity sensor

---
+++ management-server/src/sensors/bme680/humidity.ts
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the HealthyHome project management server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { NumberType, Parameter, Quality, QualityTable } from "../types";
+import Sensor from "../Sensor";
+
+/** Humidity sensor identifier */
+const HUM_SENSOR_ID = "hum";
+/** Humidity sensor textual name */
+const HUM_SENSOR_NAME = "Humidity";
+
+/** Humidity value parameter identifier */
+const HUM_VALUE_PARAM_ID = "value";
+/** Humidity value parameter textual name */
+const HUM_VALUE_PARAM_NAME = "";
+/** Humidity value parameter measurement unit */
+const HUM_VALUE_PARAM_UNIT = "%rH";
+/** Humidity value parameter value type */
+const HUM_VALUE_PARAM_TYPE = NumberType.FLOAT;
+/** Humidity value parameter quality levels table */
+const HUM_VALUE_PARAM_QUALITY_TABLE: QualityTable = [
+    [Quality.EXCELLENT, 40, 60],
+    [Quality.GOOD, 30, 70],
+    [Quality.FAIR, 20, 80],
+    [Quality.POOR, 10, 90],
+    [Quality.BAD, -Infinity, Infinity],
+];
+
+/**
+ * Humidity value parameter object
+ */
+const HUM_VALUE_PARAMETER: Parameter = {
+    id: HUM_VALUE_PARAM_ID,
+    name: HUM_VALUE_PARAM_NAME,
+    unit: HUM_VALUE_PARAM_UNIT,
+    valueType: HUM_VALUE_PARAM_TYPE,
+    qualityTable: HUM_VALUE_PARAM_QUALITY_TABLE,
+};
+
+/**
+ * Humidity sensor object
+ */
+export default new Sensor(HUM_SENSOR_ID, HUM_SENSOR_NAME, [
+    HUM_VALUE_PARAMETER,
+]);
