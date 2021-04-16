a970f1e9e7c43e1eddb31a2ac0ba526e7a52b9de
Rory Ortlepp <2196143O@student.gla.ac.uk>
2021-04-16 19:06:29 +0100

    monit/bme680: Create IAQMessage

---
+++ monitoring-server/libs/bme680/include/bme680/IAQMessage.h
@@ -0,0 +1,60 @@
+/*
+ * This file is part of the HealthyHome project monitoring server
+ * available at <https://www.github.com/healthyhomeuk/healthyhome>.
+ *
+ * Copyright (C) 2021 the authors of the HealthyHome project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file bme680/IAQMessage.h
+ */
+
+#ifndef BME680_IAQ_MESSAGE_H
+#define BME680_IAQ_MESSAGE_H
+
+#include <bme680/BSECSensorMessage.h>
+#include <core/Device.h>
+#include <core/Message.h>
+
+/**
+ * @brief Namespace for the Bosch Sensortec BME680 library.
+ */
+namespace BME680 {
+
+/**
+ * @brief Message carrying the IAQ sensor values
+ * @headerfile bme680/IAQMessage.h <bme680/IAQMessage.h>
+ */
+struct IAQMessage : public BSECSensorMessage {
+    /// Sets entity
+    SET_ENTITY = SENSOR_ENTITY("iaq");
+    /// Sets subject
+    SET_SUBJECT = "update";
+    /// Common Message codebase
+    MAKE_MESSAGE(IAQMessage);
+    /// Static deserializer codebase
+    STATIC_DESERIALIZER(IAQMessage);
+    /// Static serializer codebase
+    STATIC_SERIALIZER(IAQMessage);
+
+    static std::unique_ptr<BSECSensorMessage> bsecSensorFactory()
+    {
+        return std::make_unique<IAQMessage>();
+    }
+};
+
+}
+
+#endif // BME680_IAQ_MESSAGE_H
