b19d481e12d73994d8ea76b38ad7b4fa9afa7533
Luca Vizzarro <2252593v@student.gla.ac.uk>
2021-04-17 22:46:30 +0100

    mgmt: Fix bundler script

--- management-server/bundle.sh
+++ management-server/bundle.sh
@@ -5,6 +5,7 @@ npm install
 rm -rf ./dist/
 npm run build
 BUNDLE_FOLDER=./mgmtd-1.0.0
+mkdir -p ${BUNDLE_FOLDER}/usr/local/share
 mv ./dist ${BUNDLE_FOLDER}/usr/local/share/mgmtd
 cp package.json package-lock.json start.sh ${BUNDLE_FOLDER}/usr/local/share/mgmtd
 chmod +x ${BUNDLE_FOLDER}/usr/local/share/mgmtd/start.sh
@@ -17,3 +18,4 @@ mkdir -p ${BUNDLE_FOLDER}/etc/avahi/services
 cp avahi.service ${BUNDLE_FOLDER}/etc/avahi/services/mgmtd.service
 mkdir -p ${BUNDLE_FOLDER}/var/lib/mgmtd
 dpkg-deb --build ${BUNDLE_FOLDER}
+rm -rf ${BUNDLE_FOLDER}
